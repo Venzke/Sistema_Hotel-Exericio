79228093b5a15024e228083fefca4efee966a393
Venzke <67403605+Venzke@users.noreply.github.com>
2020-12-22 16:41:03 -0300

    Add files via upload

---
+++ Entities/Client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Programa_Hotel.Entities
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public Int64 NumberCel { get; set; }
+        public int NumberRoom { get; set; }
+
+
+        public Client(string name, int age, Int64 numberCel, int numberRoom)
+        {
+            Name = name;
+            Age = age;
+            NumberCel = numberCel;
+            NumberRoom = numberRoom;
+        }
+
+        public Client(string name, int age, string email, Int64 numberCel, int numberRoom)
+        {
+            Name = name;
+            Age = age;
+            Email = email;
+            NumberCel = numberCel;
+            NumberRoom = numberRoom;
+        }
+        public override string ToString()
+        {
+            return "Room " + NumberRoom + ": " + Name + ", " + Age + " age, " + "contact: "
+                + NumberCel + ", email: " + Email;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using Programa_Hotel.Entities;
+
+namespace Programa_Hotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<Client> list = new List<Client>();
+
+                Console.WriteLine("Welcome to Hotel: ");
+                Console.WriteLine("\n1 - Add client.");
+                Console.WriteLine("2 - Remove client."); //Menu 
+                Console.WriteLine("3 - List rooms.");
+                Console.WriteLine("4 - Exit.");
+
+                Console.Write("Select: ");
+                int op = int.Parse(Console.ReadLine());
+                char op2 = 's';
+                while (op2 == 's') //Loop
+                {
+                    switch (op) //Switch for options
+                    {
+                        case 1: // firts option of menu selected;
+
+                            Console.WriteLine("\n\t\t.:Add client:.");
+                            Console.Write("Insert name: ");
+                            string name = Console.ReadLine();
+
+                            Console.Write("Insert age: ");
+                            int age = int.Parse(Console.ReadLine());
+
+                            Console.Write("Do you have a email (s/n)? ");
+                            char op3 = char.Parse(Console.ReadLine());
+
+                            if (op3 == 's') // If for know if client have a email.
+                            {
+                                Console.Write("Insert email: ");
+                                string email = Console.ReadLine();
+
+                                Console.Write("Insert cell phone: ");
+                                Int64 numberCel = Int64.Parse(Console.ReadLine());
+
+                                Console.Write("Insert room: ");
+                                int room = int.Parse(Console.ReadLine());
+
+
+                                list.Add(new Client(name, age, email, numberCel, room));
+
+                            }
+                            else // case the client dont have a email. 
+                            {
+                                Console.Write("Insert cell phone: ");
+                                Int64 numberCel = Int64.Parse(Console.ReadLine());
+
+                                Console.Write("Insert room: ");
+                                int room = int.Parse(Console.ReadLine());
+
+                                list.Add(new Client(name, age, "XXXX-XXXX", numberCel, room));
+
+                            }
+                            break;
+
+                        case 2: // second option of menu selected;
+
+                            Console.Write("Enter the number room to delete reservation: ");
+                            int deleteReservation = int.Parse(Console.ReadLine());
+
+                            list.RemoveAt(deleteReservation);
+
+                            break;
+
+                        case 3: // third option of menu selected;
+
+                            foreach (Client obj in list)
+                            {
+                                Console.WriteLine(obj); // list the costumers
+                            }
+
+                            break;
+
+                        case 4: // fourth option of menu selected.
+
+                            Environment.Exit(0); //exit 
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid command."); // message case selected a option invalid.
+                            break;
+                    }
+
+
+                    Console.Write("\nDo you wish select other option (s/n)?");
+                    op2 = char.Parse(Console.ReadLine());
+
+                    if (op2 == 's')
+                    {
+
+                        Console.WriteLine("\n1 - Add client.");
+                        Console.WriteLine("2 - Remove client.");
+                        Console.WriteLine("3 - List rooms.");
+                        Console.WriteLine("4 - Exit.");
+
+                        Console.Write("Select: ");
+                        op = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                    }
+                }
+
+                Console.Read();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("\nError occurred: {0}", e.Message);
+                Console.Read();
+            }
+            }
+    }
+}
